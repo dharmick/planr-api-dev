c176b7936afa0c0caf2ee65834b0f7b96e0de052
dharmik <dharmikjoshi98@gmail.com>
2019-12-08 14:04:31 +0530

    login api done

--- views/authentication.py
+++ views/authentication.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request, jsonify
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 authentication_bp = Blueprint('authentication_bp', __name__)
 
 from models import Users
-from app import db
+from app import db, app
 
 
 # ====================
@@ -50,4 +52,44 @@ def user_signup():
 # ====================
 #     USER LOGIN
 # ====================
+
+@authentication_bp.route('/login', methods=['POST'])
+def user_login():
+    data = request.get_json()
+
+    user = Users.query.filter_by(email=data['email']).first()
+
+    # user not found
+    if not user:
+        return jsonify(
+            {
+                'success': False,
+                'message': 'Authentication failed'
+            }
+        ), 401
+
+    # password matched
+    if check_password_hash(user.password, data['password']):
+        token = jwt.encode(
+            {
+                'public_id': user.public_id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            },
+            app.config['SECRET_KEY']
+        )
+        return jsonify(
+            {
+                'success': True,
+                'message': 'Login successfull',
+                'token': token
+            }
+        )
+
+    # password not matched
+    return jsonify(
+        {
+            'success': False,
+            'message': 'Authentication failed'
+        }
+    ), 401
 
