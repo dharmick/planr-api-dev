2037be4beab9b860188ac8a46c120f88176f3e75
dharmik <dharmikjoshi98@gmail.com>
2019-12-14 23:47:33 +0530

    pbdfs completed with matplotlib.

--- algorithms/pbdfs/pbdfs.py
+++ algorithms/pbdfs/pbdfs.py
@@ -1,6 +1,6 @@
-# STRUCTURE OF A NODE
+# STRUCTURE OF TREE
 #
-# nodes = {
+# tree = {
 #     'ABC': {
 #         'happiness': 23,
 #         'categories': ['adventure', 'parks'],
@@ -19,15 +19,26 @@
 #     }
 # }
 
-from pbdfs_input_map import distance_matrix, pois, departure_time, time_budget, user_ratings, source, destination
+import copy
+from pprint import pprint
+import math
+import matplotlib.pyplot as plt
+from pbdfs_input_map import pois, departure_time, time_budget, user_ratings, source, destination
+
 output = {
     'route': [],
-    'happiness': 0
+    'happiness': 0,
+    'starting_time_of_ending_poi': 0,
 }
+
+tree = {}
+
+distance_matrix = {}
 
 # is it possible to go from i to j, get full service at j and still reach destination within time budget?
 def sat(i, j, starting_i):
-    reaching_j = starting_i + pois[i]['time_to_spend'] + distance_matrix[i][j],
+    # print("in sat with ",i,j,starting_i)
+    reaching_j = starting_i + pois[i]['time_to_spend'] + distance_matrix[i][j]
     opening_j = pois[j]['opening_time']
     starting_j = max(reaching_j, opening_j)
 
@@ -41,8 +52,138 @@ def sat(i, j, starting_i):
     else:
         return False
 
+# prefix of element i in set j
+def prefix(i, j):
+    res = []
+    for element in j:
+        if element != i:
+            res.append(element)
+        else:
+            return res
 
-# def prefixDFS(ordered_remaining_pois, current_node):
+
+# main recursive algo
+def prefixDFS(ordered_remaining_pois, current_node):
+    # print("\n\n*************************\n\n")
+    # print('tree is ', tree)
+    # print("ordered remainin pois: ", ordered_remaining_pois)
+    # print("current node: ", current_node)
+    for poi in ordered_remaining_pois:
+        child_ordered_remaining_pois = prefix(poi, ordered_remaining_pois)
+        child_node = copy.copy(current_node)
+        child_node.append(poi)
+
+        # print("poi: ", poi)
+
+        # print("child rem nodes: ", child_ordered_remaining_pois),
+        # print("child node: ", child_node)
+
+        if len(child_node) == 1:
+            if sat(source, poi, departure_time):
+                # print("sat returned true")
+                node = {}
+                node['happiness'] = user_ratings[poi]
+                node['categories'] = [pois[poi]['category']]
+                node['feasible_routes'] = [
+                    {
+                        'route': [source, poi],
+                        'ending_poi': poi,
+                        'starting_time_of_ending_poi': max(departure_time + distance_matrix[source][poi], pois[poi]['opening_time'])
+                    }
+                ]
+                tree["*".join(str(x) for x in child_node)] = node
+                prefixDFS(child_ordered_remaining_pois, child_node)
+
+        else:
+            for k in child_node:
+                temp_child_node = copy.copy(child_node)
+                temp_child_node.remove(k)
+
+                if "*".join(str(x) for x in temp_child_node) in tree:
+                    for l in tree["*".join(str(x) for x in temp_child_node)]['feasible_routes']:
+                        if sat(l['ending_poi'], k, l['starting_time_of_ending_poi']):
+                            child_node_string = "*".join(str(x) for x in child_node)
+                            temp_child_node_string = "*".join(str(x) for x in temp_child_node)
+
+                            new_happiness = tree[temp_child_node_string]['happiness'] + user_ratings[k]
+                            new_categories = copy.copy(tree[temp_child_node_string]['categories'])
+                            new_categories.append(pois[k]['category'])
+                            new_starting_time_of_ending_poi = max(l['starting_time_of_ending_poi'] + pois[l['ending_poi']]['time_to_spend'] + distance_matrix[l['ending_poi']][k], pois[k]['opening_time'])
+                            new_path = copy.copy(l['route'])
+                            new_path.append(k)
+                            new_route = {
+                                    'route': new_path,
+                                    'ending_poi': k,
+                                    'starting_time_of_ending_poi': new_starting_time_of_ending_poi
+                                }
+
+                            if child_node_string in tree:
+                                tree[child_node_string]['happiness'] = new_happiness
+                                tree[child_node_string]['categories'] = new_categories
+                                tree[child_node_string]['feasible_routes'].append(new_route)
+                            else:
+                                new_node = {}
+                                new_node['happiness'] = new_happiness
+                                new_node['categories'] = new_categories
+                                new_node['feasible_routes'] = [new_route]
+                                tree[child_node_string] = new_node
+
+                            if (new_happiness > output['happiness']) or \
+                            (new_happiness == output['happiness'] and \
+                            new_starting_time_of_ending_poi < output['starting_time_of_ending_poi']):
+                                output['route'] = new_path
+                                output['happiness'] = new_happiness
+                                output['starting_time_of_ending_poi'] = new_starting_time_of_ending_poi
+
+
+            if "*".join(str(x) for x in temp_child_node) in tree:
+                prefixDFS(child_ordered_remaining_pois, child_node)
+    # print("ended")
+
 
 
 if __name__ == '__main__':
+    places = list(user_ratings.keys())
+
+    for from_place in places:
+        distance_matrix[from_place] = {}
+        for to_place in places:
+            distance_matrix[from_place][to_place] =  math.sqrt((pois[from_place]['latitude'] - pois[to_place]['latitude'])**2 + \
+            (pois[from_place]['longitude'] - pois[to_place]['longitude'])**2) / 20
+
+    # pprint(distance_matrix)
+
+
+    places.remove(source)
+    if destination in places:
+        # check required because source and destination can be equal
+        places.remove(destination)
+    sorted_places = sorted(places, key=user_ratings.get, reverse=True)
+    prefixDFS(sorted_places, [])
+
+    x = []
+    y = []
+    text = []
+    for poi in pois:
+        x.append(pois[poi]['longitude'])
+        y.append(pois[poi]['latitude'])
+        text.append(str(poi) + " (" + str(user_ratings[poi]) + ")")
+
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)
+    for i, txt in enumerate(text):
+        ax.annotate(txt, (x[i], y[i]))
+
+    line_x = []
+    line_y = []
+    for poi in output['route']:
+        line_x.append(pois[poi]['longitude'])
+        line_y.append(pois[poi]['latitude'])
+    line_x.append(pois[destination]['longitude'])
+    line_y.append(pois[destination]['latitude'])
+    plt.plot(line_x, line_y)
+
+    # pprint(tree)
+    print(output)
+
+    plt.show()
--- algorithms/pbdfs/pbdfs_input_map.py
+++ algorithms/pbdfs/pbdfs_input_map.py
@@ -1,5 +1,5 @@
-source = 143,
-destination = 143
+source = 1232
+destination = 887
 
 # starting from morning 9 AM
 departure_time = 9
@@ -13,9 +13,24 @@ time_budget = 12
 user_ratings = {
     143: 3.556,
     2666: 4.007,
-    345: 1.2223,
-    4221: 4.996,
+    345: 4.2223,
+    4221: 0.996,
     23: 0.7445,
+    445: 2.556,
+    887: 4.507,
+    3: 3.0223,
+    666: 1.996,
+    1232: 4.7445,
+    2: 4.556,
+    50: 1.007,
+    2231: 4.2223,
+    99: 1.996,
+    100: 4.7445,
+    101: 0.556,
+    509: 4.507,
+    9997: 3.3223,
+    890: 1.996,
+    3359: 4.9445,
 }
 
 pois = {
@@ -25,20 +40,23 @@ pois = {
         'opening_time': 10,
         'closing_time': 18,
         'time_to_spend': 1,
+        'category': 'Adventure'
     },
     2666:{
         'latitude': 10,
-        'longitude': 15,
+        'longitude': 30,
         'opening_time': 9,
         'closing_time': 21,
         'time_to_spend': 2,
+        'category': 'Parks'
     },
     345:{
-        'latitude': 20,
+        'latitude': 50,
         'longitude': 20,
         'opening_time': 12,
         'closing_time': 18,
         'time_to_spend': 1.5,
+        'category': 'Adventure'
     },
     4221:{
         'latitude': 5,
@@ -46,6 +64,7 @@ pois = {
         'opening_time': 10,
         'closing_time': 19,
         'time_to_spend': 2,
+        'category': 'Entertainment'
     },
     23:{
         'latitude': 80,
@@ -53,43 +72,259 @@ pois = {
         'opening_time': 10,
         'closing_time': 18,
         'time_to_spend': 1,
+        'category': 'Nature'
+    },
+    445:{
+        'latitude': 45,
+        'longitude': 60,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 1,
+        'category': 'Entertainment'
+    },
+    887:{
+        'latitude': 60,
+        'longitude': 80,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 2.5,
+        'category': 'Nature'
+    },
+    3:{
+        'latitude': 25,
+        'longitude': 50,
+        'opening_time': 9,
+        'closing_time': 18,
+        'time_to_spend': 1.5,
+        'category': 'Adventure'
+    },
+    666:{
+        'latitude': 70,
+        'longitude': 20,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 1,
+        'category': 'Nature'
+    },
+    1232:{
+        'latitude': 30,
+        'longitude': 30,
+        'opening_time': 10,
+        'closing_time': 19,
+        'time_to_spend': 2,
+        'category': 'Parks'
+    },
+    2:{
+        'latitude': 15,
+        'longitude': 70,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 1,
+        'category': 'Adventure'
+    },
+    50:{
+        'latitude': 45,
+        'longitude': 88,
+        'opening_time': 9,
+        'closing_time': 21,
+        'time_to_spend': 2,
+        'category': 'Parks'
+    },
+    2231:{
+        'latitude': 24,
+        'longitude': 60,
+        'opening_time': 12,
+        'closing_time': 18,
+        'time_to_spend': 1.5,
+        'category': 'Adventure'
+    },
+    99:{
+        'latitude': 59,
+        'longitude': 59,
+        'opening_time': 10,
+        'closing_time': 19,
+        'time_to_spend': 2,
+        'category': 'Entertainment'
+    },
+    100:{
+        'latitude': 30,
+        'longitude': 58,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 1,
+        'category': 'Nature'
+    },
+    101:{
+        'latitude': 40,
+        'longitude': 88,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 1,
+        'category': 'Entertainment'
+    },
+    509:{
+        'latitude': 90,
+        'longitude': 10,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 2.5,
+        'category': 'Nature'
+    },
+    9997:{
+        'latitude': 85,
+        'longitude': 30,
+        'opening_time': 9,
+        'closing_time': 18,
+        'time_to_spend': 1.5,
+        'category': 'Adventure'
+    },
+    890:{
+        'latitude': 24,
+        'longitude': 83,
+        'opening_time': 10,
+        'closing_time': 18,
+        'time_to_spend': 1,
+        'category': 'Nature'
+    },
+    3359:{
+        'latitude': 10,
+        'longitude': 90,
+        'opening_time': 10,
+        'closing_time': 19,
+        'time_to_spend': 2,
+        'category': 'Parks'
     }
 }
 
-distance_matrix = {
-    143: {
-        143: 0,
-        2666: 0.5,
-        345: 1,
-        4221: 3,
-        23: 5.5,
-    },
-    2666: {
-        143: 0.5,
-        2666: 0,
-        345: 0.8,
-        4221: 2.5,
-        23: 5.5,
-    },
-    345: {
-        143: 1,
-        2666: 0.8,
-        345: 0,
-        4221: 2.5,
-        23: 4,
-    },
-    4221: {
-        143: 3,
-        2666: 2.5,
-        345: 2.5,
-        4221: 0,
-        23: 3.5,
-    },
-    23: {
-        143: 5.5,
-        2666: 5.5,
-        345: 4,
-        4221: 3.5,
-        23: 0,
-    }
-}
+# distance_matrix = {
+#     143: {
+#         143: 0,
+#         2666: 1,
+#         345: 2,
+#         4221: 2,
+#         23: 5.5,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     2666: {
+#         143: 1,
+#         2666: 0,
+#         345: 0.8,
+#         4221: 2.5,
+#         23: 5.5,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     345: {
+#         143: 2,
+#         2666: 0.8,
+#         345: 0,
+#         4221: 2.5,
+#         23: 4,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     4221: {
+#         143: 3,
+#         2666: 2.5,
+#         345: 2.5,
+#         4221: 0,
+#         23: 3.5,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     23: {
+#         143: 5.5,
+#         2666: 5.5,
+#         345: 4,
+#         4221: 3.5,
+#         23: 0,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     445: {
+#         143: 5.5,
+#         2666: 5.5,
+#         345: 4,
+#         4221: 3.5,
+#         23: 0,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     887: {
+#         143: 5.5,
+#         2666: 5.5,
+#         345: 4,
+#         4221: 3.5,
+#         23: 0,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     3: {
+#         143: 5.5,
+#         2666: 5.5,
+#         345: 4,
+#         4221: 3.5,
+#         23: 0,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     666: {
+#         143: 5.5,
+#         2666: 5.5,
+#         345: 4,
+#         4221: 3.5,
+#         23: 0,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     },
+#     1232: {
+#         143: 5.5,
+#         2666: 5.5,
+#         345: 4,
+#         4221: 3.5,
+#         23: 0,
+
+#         445: 2.556,
+#         887: 4.507,
+#         3: 3.0223,
+#         666: 1.996,
+#         1232: 4.7445,
+#     }
+# }
